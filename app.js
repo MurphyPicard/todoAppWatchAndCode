9e0d60449d218195fbe0104d168a0c6f8115f63d
Ara <ayaghsizian@intracitygeeks.org>
2017-07-22 11:21:51 -0400

    object with methods added

---
+++ app.js
@@ -0,0 +1,28 @@
+var todolist = {
+
+  todos: ['item 1', 'item 2', 'item 3'],
+
+  displayTodos: function(){
+    console.log('My Todos: ', this.todos);
+  },
+
+  addTodo: function(todo){
+    this.todos.push(todo);
+    this.displayTodos();
+  },
+
+  changeTodo: function(pos, newValue){
+    this.todos[pos] = newValue;
+    this.displayTodos();
+  },
+
+  deleteTodo: function(pos){
+    this.todos.splice(pos, 1);
+    this.displayTodos();
+  }
+};
+
+{
+  todoText: 'item 1',
+  completed: true
+}
