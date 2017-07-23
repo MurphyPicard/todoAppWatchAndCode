6bda49af8fae7173485442d74222d566f9b73971
Ara <ayaghsizian@intracitygeeks.org>
2017-07-23 00:18:48 -0400

    deleted todolist.displayTodos everywhere because unneeded

--- app.js
+++ app.js
@@ -1,44 +1,24 @@
 var todolist = {
   todos: [],
-  // displays the list
-  displayTodos: function(){
-    // debugger;
-    if(this.todos.length < 1){
-      console.log("There are no todos yet.  Use addTodo if you want.");
-    }else{
-      console.log('My Todos: ');
-      for(var i = 0; i < this.todos.length; i++){
-        if(this.todos[i].completed === true){
-          console.log("(x)", this.todos[i].todoText);
-        }else{
-          console.log("( )", this.todos[i].todoText);
-        }//else
-      }//for
-    }//else
-  },
   // adds a todo to the end of the list
   addTodo: function(todoText){
     this.todos.push({
       todoText: todoText,
       completed: false
     });
-    this.displayTodos();
   },
   // changes a todo
   changeTodo: function(pos, todoText){
     this.todos[pos].todoText = todoText;
-    this.displayTodos();
   },
   // deletes a todo
   deleteTodo: function(pos){
     this.todos.splice(pos, 1);
-    this.displayTodos();
   },
   // change if completed
   toggleCompleted: function(pos) {
     var todo = this.todos[pos];
     todo.completed = !todo.completed;
-    this.displayTodos();
   },
   // change all to true or false
   toggleAll: function(){
@@ -55,16 +35,12 @@ var todolist = {
       for (var i = 0; i < totalTodos; i++) {
         this.todos[i].completed = false;
       }
-      console.log("all false");
-      this.displayTodos();
     }
     // change all to true
     else{
       for (var i = 0; i < totalTodos; i++) {
         this.todos[i].completed = true;
       }
-      console.log("all true");
-      this.displayTodos();
     }
   }
 };
